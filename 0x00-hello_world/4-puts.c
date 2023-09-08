fab21449dcc38db23f61ae71440f1c5c8a493001
Desalegn07 <desalegn07116@gmail.com>
2023-09-08 01:18:34 -0700

    using puts'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return:0
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Prorogramming is like building a multilingual puzzle");
 return (0);
 }
