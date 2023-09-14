304713e7df20cc1ad3ada2b86ab18cbb61606ae0
Desalegn07 <desalegn07116@gmail.com>
2023-09-14 10:39:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower.c - Check if char is lowercase.
+* @c: is the char to be checked
+*Return: 1 if the char is lowercase, othereise 0.
+*/
+int_islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+returni(0);
+}
