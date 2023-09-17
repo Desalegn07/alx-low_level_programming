7e14a05968c6f9adb92ec3a5f2a03dea1081a4cb
Desalegn07 <desalegn07116@gmail.com>
2023-09-17 10:24:59 -0700

    6 print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,30 +7,10 @@
  */
 void print_line(int n)
 {
-if (n == 0)
-{
-_putchar('\n');
-}
-else if (n > 0)
+while (n > 0)
 {
 _putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('\n');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('_');
-_putchar('\0');
+n--;
 }
-else
-{
 _putchar('\n');
-}
 }
