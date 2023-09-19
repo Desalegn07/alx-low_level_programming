515e72473fdf139de8c5145912bd4f24e1f2b4d8
Desalegn07 <desalegn07116@gmail.com>
2023-09-19 13:14:24 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: points to characters.
+ * return: the string
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\0');
+}
