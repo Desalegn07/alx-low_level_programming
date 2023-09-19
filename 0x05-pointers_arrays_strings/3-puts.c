b3668d970ade376eccf67f189bcbd032250bb850
Desalegn07 <desalegn07116@gmail.com>
2023-09-19 13:53:30 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,14 @@
 
 /**
  * _puts - Prints a string, followed by a new line, to stdout.
- * @str: points to characters.
- * return: the string
+ * @str: prints a string
+ * return: void
  */
 void _puts(char *str)
 {
 int j;
 
 for (j = 0; str[j] != '\0'; j++)
-{
 _putchar(str[j]);
-_putchar('\0');
-}
+_putchar('\n');
 }
