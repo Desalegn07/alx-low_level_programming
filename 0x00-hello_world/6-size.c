b41660819202aaa01e83abc205a552a269f2c54e
Desalegn07 <desalegn07116@gmail.com>
2023-10-02 11:41:39 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,23 +3,17 @@
 #include <limits.h>
 #include <float.h>
 
-int main(int argc, char** argv) {
+int main(void)
 
-    printf("CHAR_BIT    :   %d\n", CHAR_BIT);
-    printf("CHAR_MAX    :   %d\n", CHAR_MAX);
-    printf("CHAR_MIN    :   %d\n", CHAR_MIN);
-    printf("INT_MAX     :   %d\n", INT_MAX);
-    printf("INT_MIN     :   %d\n", INT_MIN);
-    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
-    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
-    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
-    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
-    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
-    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
-    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
-    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
-    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
-    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of short: %zu byte(s)\n", sizeof(short));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long: %zu byte(s)\n", sizeof(long));
+printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
+printf("Size of double: %zu byte(s)\n", sizeof(double));
+printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 
-    return 0;
+return (0);
 }
